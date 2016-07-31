17bc32af67b82b1d329b14345ae4aab8c7277503
markmasterson <mark.masterson@kenandy.com>
2016-07-30 22:18:22 -0700

    ReduxConnection Interface added. Components now work with access upgrade turned on.

--- src/aura/Provider/ProviderController.js
+++ src/aura/Provider/ProviderController.js
@@ -3,11 +3,15 @@
         var children = component.get("v.body");
         var store = component.get("v.store");
         children.forEach(function(child) {
-            child.set("v.store", store);
+            if(child.isInstanceOf("c:ReduxConnection")) {
+                child.set("v.store", store);
+            }
         });
         function handleChanges() {
             children.forEach(function(child) {
-                child.set("v.store", store);
+                if(child.isInstanceOf("c:ReduxConnection")) {
+                    child.set("v.store", store);
+                }
             });
         }
         store.subscribe(handleChanges);
