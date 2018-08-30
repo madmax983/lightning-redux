b5c4ecba0492976b4564c944f004b8c2000b6263
mmasterson <mmasterson@fusionrm.com>
2018-08-30 12:39:15 -0500

    #17 Test for unsubscribe on destroy.

--- force-app/main/default/aura/Redux/ReduxController.js
+++ force-app/main/default/aura/Redux/ReduxController.js
@@ -16,6 +16,7 @@
 
             if(window.reducerQueue && window.reducerQueue[reduxName]) {
                 combinedReducer = Redux.combineReducers(Object.assign({}, reducerObject, window.reducerQueue[reduxName]));
+                window.reducerRegistry = {};
                 window.reducerRegistry[reduxName] = Object.assign({}, window.reducerRegistry[reduxName], reducerObject, window.reducerQueue[reduxName]);
             } else if(window.reducerRegistry && window.reducerRegistry[reduxName]) {
                 combinedReducer = Redux.combineReducers(reducerObject);
@@ -90,7 +91,7 @@
         var listener = params ? params.listener : null;
 
         if(listener && window.reduxStore && window.reduxStore[reduxName]) {
-            window.reduxStore[reduxName].subscribe(listener);
+            return window.reduxStore[reduxName].subscribe(listener);
         }
     },
 
@@ -147,12 +148,7 @@
                     for(var key in mapStateToAttributes) {
                         if(mapStateToAttributes.hasOwnProperty(key)) {
                             if(typeof mapStateToAttributes[key] === "function") {
-                                try {
-                                    var returnedFunction = mapStateToAttributes[key]();
-                                    target.set(key, returnedFunction(state, target));
-                                } catch(e) {
-                                    target.set(key, mapStateToAttributes[key](state, target))
-                                }
+                                target.set(key, mapStateToAttributes[key](state, target))
                             } else {
                                 target.set(key, state[mapStateToAttributes[key]]);
                             }
